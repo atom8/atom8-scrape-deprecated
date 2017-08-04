91c5fcb95f754c108b2107ec2c691d0c971d9525
Sheldon Allen <a.sheldon.sol@gmail.com>
2017-08-03 22:48:50 -0400

    Added click library

--- scraper.py
+++ scraper.py
@@ -1,22 +1,12 @@
-#!/usr/bin/python3
-
 import click
-import time
 import datetime
-import reddit
 import json
 import os
-import urllib.request
+import sys
+import time
 
-
-# TODO get median karma
-# pull every post in top 25% of karma
-# Research subs:
-
-# author
-# date
-# content
-# title
+DESKTOP_IDENTIFIER = 'desktop*'
+SETTINGS = None
 
 
 def download_image_from_url(url, directory, filename=None):
@@ -33,30 +23,55 @@ def download_image_from_url(url, directory, filename=None):
     urllib.request.urlretrieve(url, os.path.join(directory, filename))
 
 
-def main():
-    # TODO allow for specifying output directory
+def get_scraper_settings(settings_filename):
+    if SETTINGS is None:
+        with open(settings_filename) as settings_file:
+            SETTINGS = json.load(settings_file)
+    return SETTINGS
+
+
+@click.group()
+def scraper():
+    pass
+
+@scraper.command()
+@click.option('--export_directory', prompt='Output directory', type=str, default=DESKTOP_IDENTIFIER)
+def all(export_directory):
+    # Find desktop path
+    if export_directory == DESKTOP_IDENTIFIER:
+        if sys.platform.startswith('win'):
+            export_directory = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
+        elif sys.platform.startswith('linux'):
+            export_directory = os.path.join(os.path.join(os.path.expanduser('~')), 'Desktop')
+        else:
+            return  # TODO properly exit
+
+    # Create export folder, with timestamp
     t = time.time()
     timestamp = datetime.datetime.fromtimestamp(t).strftime('%Y%m%d_%H%M%S')
-    export_directory = os.path.join(os.getcwd(), 'export' + timestamp)
+    export_folder = os.path.join(export_directory, 'export' + timestamp)
     try:
-        os.makedirs(export_directory, exist_ok=False)
+        os.makedirs(export_folder, exist_ok=False)
     except OSError:
-        if (os.listdir(export_directory)):
-            print('[ERROR] Export directory already exists')
+        if (os.listdir(export_folder)):
+            print('[ERROR] Export folder already exists')
             return
-    print('Exporting to ' + export_directory)
+    print('Exporting to ' + export_folder)
 
-    # Retrieve settings
-    settings = None
-    with open('settings.json') as settings_file:
-        settings = json.load(settings_file)
-    SUBREDDITS = settings['subreddits']
+    # perform scrapes
+    reddit()
+
+
+@scraper.command()
+def reddit():
+    settings = get_scraper_settings()
+    subreddits = settings['subreddits']
 
     # Retrieve reddit posts
     # TODO different date ranges
     reddit_posts = []
     for subreddit in SUBREDDITS:
-        subreddit_posts = reddit.get_posts_by_date(
+        subreddit_posts = reddit_cron.get_posts_by_date(
             subreddit=subreddit['name'],
             min_score=subreddit['min_karma'],
             days=7,
@@ -81,5 +96,5 @@ def main():
             pass
 
 
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    scraper()
