60fbae21ebd6d090bd9c7e09aae44797511ac007
Sheldon Allen <a.sheldon.sol@gmail.com>
2017-08-03 23:44:27 -0400

    Updated reddit scrape

--- scraper.py
+++ scraper.py
@@ -3,7 +3,9 @@ import datetime
 import json
 import os
 import sys
+import reddit as reddit_control
 import time
+import urllib
 
 DESKTOP_IDENTIFIER = 'desktop*'
 SETTINGS = None
@@ -24,19 +26,25 @@ def download_image_from_url(url, directory, filename=None):
 
 
 def get_scraper_settings(settings_filename):
+    global SETTINGS
+
     if SETTINGS is None:
         with open(settings_filename) as settings_file:
             SETTINGS = json.load(settings_file)
     return SETTINGS
 
 
-@click.group()
-def scraper():
-    pass
+def create_folder(export_folder):
+    try:
+        os.makedirs(export_folder, exist_ok=False)
+    except OSError:
+        if (os.listdir(export_folder)):
+            print('[ERROR] Export folder already exists')
 
-@scraper.command()
-@click.option('--export_directory', prompt='Output directory', type=str, default=DESKTOP_IDENTIFIER)
-def all(export_directory):
+
+def get_export_folder(prefix='export'):
+    export_directory = click.prompt('Export directory', type=str, default=DESKTOP_IDENTIFIER)
+
     # Find desktop path
     if export_directory == DESKTOP_IDENTIFIER:
         if sys.platform.startswith('win'):
@@ -44,34 +52,24 @@ def all(export_directory):
         elif sys.platform.startswith('linux'):
             export_directory = os.path.join(os.path.join(os.path.expanduser('~')), 'Desktop')
         else:
-            return  # TODO properly exit
+            # TODO apple
+            print("Cannot find desktop, using working directory")
+            export_directory = os.cwd()
 
     # Create export folder, with timestamp
     t = time.time()
     timestamp = datetime.datetime.fromtimestamp(t).strftime('%Y%m%d_%H%M%S')
-    export_folder = os.path.join(export_directory, 'export' + timestamp)
-    try:
-        os.makedirs(export_folder, exist_ok=False)
-    except OSError:
-        if (os.listdir(export_folder)):
-            print('[ERROR] Export folder already exists')
-            return
-    print('Exporting to ' + export_folder)
-
-    # perform scrapes
-    reddit()
+    return os.path.join(export_directory, prefix + timestamp)
 
 
-@scraper.command()
-def reddit():
-    settings = get_scraper_settings()
+def scrape_reddit(settings, export_folder, verbose=False):
     subreddits = settings['subreddits']
 
     # Retrieve reddit posts
     # TODO different date ranges
     reddit_posts = []
-    for subreddit in SUBREDDITS:
-        subreddit_posts = reddit_cron.get_posts_by_date(
+    for subreddit in subreddits:
+        subreddit_posts = reddit_control.get_posts_by_date(
             subreddit=subreddit['name'],
             min_score=subreddit['min_karma'],
             days=7,
@@ -81,7 +79,7 @@ def reddit():
 
     # Export post data
     # TODO sort posts by highest karma
-    with open(os.path.join(export_directory, 'export_reddit.json'), 'w') as f:
+    with open(os.path.join(export_folder, 'export_reddit.json'), 'w') as f:
         json.dump(reddit_posts, f, indent=4)
 
     # Extract post data and export images
@@ -89,11 +87,46 @@ def reddit():
     for post in reddit_posts:
         try:
             image_url = post['url']
-            print(image_url)  # TODO if verbose
-            download_image_from_url(image_url, export_directory)
+            if verbose:
+                print(image_url)
+            
+            domain = post['domain']
+            if domain == 'gfycat.com':
+                image_url = image_url[:8] + 'zippy.' + image_url[8:] + '.webm'
+            if image_url.endswith('gifv'):
+                image_url = image_url[:-4] + 'mp4'
+
+            download_image_from_url(image_url, export_folder)
+
         except (FileNotFoundError, OSError) as e:
             print("Ignored")
             pass
+
+
+@click.group()
+def scraper():
+    pass
+
+
+@scraper.command()
+def all():
+    export_folder = get_export_folder()
+    create_folder(export_folder)
+    # create_folder(export_folder, reddit)
+
+    settings = get_scraper_settings('settings.json')
+
+    # perform scrapes
+    scrape_reddit(settings, export_folder)
+
+
+@scraper.command()
+def reddit():
+    export_folder = get_export_folder(prefix='reddit')
+    create_folder(export_folder)
+    settings = get_scraper_settings('settings.json')
+
+    scrape_reddit(settings, export_folder)
 
 
 if __name__ == "__main__":
