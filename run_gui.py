48e2880ac08192a5cc99d62a4a4361a3e195324c
Sheldon Allen <a.sheldon.sol@gmail.com>
2019-01-16 22:45:54 -0500

    Added scrollbar

--- run_gui.py
+++ run_gui.py
@@ -2,14 +2,6 @@ import tkinter as tk
 from tkinter import filedialog
 
 
-# tk.filedialog.askdirectory()
-
-
-def get_destination():
-    name = filedialog.askdirectory()
-    print(name)
-
-
 APP_WIDTH = 960
 APP_HEIGHT = 640
 
@@ -28,14 +20,22 @@ class MainApplication(tk.Frame):
         tk.Button(export_frame, text='Browse',
                   command=self.change_destination_folder).pack(side='right')
 
-        # Console
-        self.output = tk.Text(self, height=10, width=40)
-        self.output.insert(tk.END, 'Welcome to GDI scraper')
-        self.output.pack()
+        # Output diag
+        output_frame = tk.Frame(self)
+        output_frame.pack()
+        self.output = tk.Text(output_frame, height=10, width=40)
+        self.output.insert(tk.END, 'Welcome to GDI scrape\n')
+        self.output.pack(side='left', fill=tk.Y)
+        out_scroll = tk.Scrollbar(output_frame)
+        out_scroll.config(command=self.output.yview)
+        self.output.config(yscrollcommand=out_scroll.set)
+        out_scroll.pack(side='right', fill=tk.Y)
 
     def change_destination_folder(self):
         destination_folder = filedialog.askdirectory()
         self.export_label.config(text='Export to: ' + destination_folder)
+        self.output.insert(tk.END, 'Export folder changed ~ %s\n' %
+                           destination_folder)
 
 
 if __name__ == '__main__':
